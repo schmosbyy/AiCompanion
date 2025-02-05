83b6994114cc4756623aa808192329b4426c849b
schmosbyy <sakaarpoply@gmail.com>
2025-02-05 17:42:50 -0500

    get response from Ollama-deepseek

--- src/Http/Controllers/AiController.php
+++ src/Http/Controllers/AiController.php
@@ -3,12 +3,21 @@
 namespace Schmosbyy\AiCompanion\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AiController extends Controller
 {
     public function ask(Request $request)
     {
         $prompt = $request->input('user_input');
-        return "You entered: " . $prompt;
+        //command to run on a custom portOLLAMA_HOST=127.0.0.1:11435 ollama serve
+        $response = Http::post('http://127.0.0.1:11435/api/generate', [
+            'model' => 'deepseek-r1:14b',
+            'prompt' => $prompt,
+            'stream' => false
+        ]);
+        $decodedResponse = json_decode($response, true);
+        $generatedSQL = $decodedResponse['response'];
+        return "You entered: " . $generatedSQL;
     }
 }
